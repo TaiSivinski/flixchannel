dafe1e0212fef8dea1e1703bcd1f43a56483acd8
Tainara Bettoni Oliveira <tbettoni@emeal.nttdata.com>
2022-11-01 19:19:25 -0300

    first commit

---
+++ src/test/java/com/video/flixchannel/FlixchannelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.video.flixchannel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlixchannelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
